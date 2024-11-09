99fe41633111585fb1e60b366e8cb2d911ac733b
josua-stng <josua.sitanggang@radyalabs.id>
2024-11-09 10:55:08 +0700

    feat: create config for images remote pattern

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images:{
+    remotePatterns:[
+      {
+           hostname: 'www.theaudiodb.com'
+      }
+    ]
+  },
   experimental: {
     appDir: true,
   },
