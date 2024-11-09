84830403746e862cf0a74042d05f5c649f3afc99
josua-stng <josua.sitanggang@radyalabs.id>
2024-11-09 10:59:29 +0700

    feat: add album header for album detail

---
+++ src/app/views/AlbumDetail/AlbumHeader/AlbumHeader.tsx
@@ -0,0 +1,12 @@
+export default function AlbumHeader() {
+  return (
+    <header className="flex justify-between p-3">
+      <h1>Albums Detail</h1>
+      <div className="flex gap-3">
+        <p>link 1</p>
+        <p>link 2</p>
+        <p>link 3</p>
+      </div>
+    </header>
+  );
+}
