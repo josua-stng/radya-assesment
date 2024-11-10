8da736a2bad20735b43fd0b7e8b18680b1f15a13
josua-stng <josua.sitanggang@radyalabs.id>
2024-11-10 09:12:49 +0700

    fix: remove header & footer from album detail page

--- src/app/album-detail/[id]/page.tsx
+++ src/app/album-detail/[id]/page.tsx
@@ -1,5 +1,3 @@
-import AlbumFooter from '@/app/views/AlbumDetail/AlbumFooter';
-import AlbumHeader from '@/app/views/AlbumDetail/AlbumHeader';
 import AlbumInformation from '@/app/views/AlbumDetail/AlbumInformation';
 import DiscographyAlbum from '@/app/views/AlbumDetail/DiscographyAlbum';
 import NewsLetter from '@/app/views/AlbumDetail/NewsLetter';
@@ -9,12 +7,10 @@ export default async function AlbumDetail({ params }: any) {
   const { id } = await params;
   return (
     <div>
-      <AlbumHeader />
       <AlbumInformation id={id} />
       <TrackList id={id} />
       <DiscographyAlbum />
       <NewsLetter />
-      <AlbumFooter />
     </div>
   );
 }
