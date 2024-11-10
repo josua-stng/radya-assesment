cf6b980bf6f2c8f1176cb770e386eabd289b8867
josua-stng <josua.sitanggang@radyalabs.id>
2024-11-10 09:11:09 +0700

    feat: add skeleton loading bar for discography

--- src/app/views/Discography/Discography.tsx
+++ src/app/views/Discography/Discography.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import useGetData from '@/app/hooks/useGetData';
 import { Card, CardContent } from '@/components/ui/card';
 import {
@@ -10,50 +9,63 @@ import {
 import Image from 'next/image';
 import Link from 'next/link';
 import { IIDiscography, IIDiscographyResponse } from './Discography.types';
+import { DiscographySkeleton } from './components/DiscographySkeleton';
 
 export default function Discography() {
-  const { data: discography } = useGetData<IIDiscographyResponse>(
+  const { data: discography, isLoading } = useGetData<IIDiscographyResponse>(
     'https://www.theaudiodb.com/api/v1/json/2/album.php?i=111304',
     'discography'
   );
   return (
-    <Carousel className="mt-24 mb-20 w-full max-w-7xl mx-auto">
-      <h1 className="font-bold ml-2 text-xl italic mt-3 mb-1">Discography</h1>
-      <h2 className="mb-1 text-sm px-1">
-        This remix album presents a new perspective on the artist&apos;s work
-        through the artist&apos;s creative interpretation. Each song is
-        reimagined with a modern electronic twist
-      </h2>
-      <CarouselContent>
-        {discography?.album.map((discographys: IIDiscography) => {
-          return (
-            <CarouselItem
-              key={discographys.idAlbum}
-              className="pl-1 md:basis-1/4 lg:basis-1/6"
-            >
-              <Link href={`/album-detail/${discographys.idAlbum}`}>
-                <Card className="hover:bg-slate-200 cursor-pointer">
-                  <CardContent className="flex aspect-square items-center justify-center p-2">
-                    <Image
-                      src={discographys.strAlbumThumb}
-                      width={500}
-                      height={200}
-                      alt="image thumb"
-                      className="w-full h-full object-cover rounded-md"
-                    />
-                  </CardContent>
-                  <div className=" ml-3 mt-1 space-y-1 mb-2">
-                    <p className="line-clamp-1 font-bold text-sm">
-                      {discographys.strAlbum}
-                    </p>
-                    <p className="text-xs">{discographys.intYearReleased}</p>
-                  </div>
-                </Card>
-              </Link>
-            </CarouselItem>
-          );
-        })}
-      </CarouselContent>
-    </Carousel>
+    <div className="scroll-mt-20" id="discography">
+      <Carousel className="mt-24 mb-20 w-full max-w-7xl mx-auto">
+        {isLoading ? (
+          <DiscographySkeleton />
+        ) : (
+          <>
+            <h1 className="font-bold ml-2 text-xl italic mt-3 mb-1">
+              Discography
+            </h1>
+            <h2 className="mb-1 text-sm px-1">
+              This remix album presents a new perspective on the artist&apos;s
+              work through the artist&apos;s creative interpretation. Each song
+              is reimagined with a modern electronic twist
+            </h2>
+            <CarouselContent>
+              {discography?.album.map((discographyAlbum: IIDiscography) => {
+                return (
+                  <CarouselItem
+                    key={discographyAlbum.idAlbum}
+                    className="pl-1 md:basis-1/4 lg:basis-1/6"
+                  >
+                    <Link href={`/album-detail/${discographyAlbum.idAlbum}`}>
+                      <Card className="hover:bg-slate-200 cursor-pointer">
+                        <CardContent className="flex aspect-square items-center justify-center p-2">
+                          <Image
+                            src={discographyAlbum.strAlbumThumb}
+                            width={500}
+                            height={200}
+                            alt="image thumb"
+                            className="w-full h-full object-cover rounded-md"
+                          />
+                        </CardContent>
+                        <div className="ml-3 mt-1 space-y-1 mb-2">
+                          <p className="line-clamp-1 font-bold text-sm">
+                            {discographyAlbum.strAlbum}
+                          </p>
+                          <p className="text-xs">
+                            {discographyAlbum.intYearReleased}
+                          </p>
+                        </div>
+                      </Card>
+                    </Link>
+                  </CarouselItem>
+                );
+              })}
+            </CarouselContent>
+          </>
+        )}
+      </Carousel>
+    </div>
   );
 }
---
+++ src/app/views/Discography/components/DiscographySkeleton.tsx
@@ -0,0 +1,20 @@
+import { Skeleton } from '@/components/ui/skeleton';
+
+export function DiscographySkeleton() {
+  return (
+    <div>
+      <div className="mx-auto max-w-7xl">
+        <Skeleton className="h-4 w-[100px] mt-4 mb-2 ml-2 bg-gray-300 " />
+      </div>
+      <div className="flex  max-w-7xl mx-auto gap-5 items-center">
+        {Array.from({ length: 6 }).map((_, index) => (
+          <div key={index} className="p-1  space-y-2">
+            <Skeleton className="h-[125px] w-[190px] rounded-xl bg-gray-300" />
+            <Skeleton className="h-4 w-[150px] bg-gray-300" />
+            <Skeleton className="h-4 w-[100px] bg-gray-300" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
