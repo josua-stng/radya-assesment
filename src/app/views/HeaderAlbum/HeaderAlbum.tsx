27cd6fbeea413de1267dbad262363eb8d694cdd1
josua-stng <josua.sitanggang@radyalabs.id>
2024-11-10 09:10:26 +0700

    feat: add href for content item header

--- src/app/views/HeaderAlbum/HeaderAlbum.tsx
+++ src/app/views/HeaderAlbum/HeaderAlbum.tsx
@@ -1,11 +1,21 @@
+import Link from 'next/link';
+
 export default function HeaderAlbum() {
   return (
-    <header className="flex justify-between p-3">
-      <h1>Albums</h1>
-      <div className="flex gap-3">
-        <p>link 1</p>
-        <p>link 2</p>
-        <p>link 3</p>
+    <header className="flex sticky top-0 z-20 justify-between p-5 font-sans font-semibold bg-gray-300">
+      <Link href={'/'} className="hover:underline cursor-pointer">
+        Albums
+      </Link>
+      <div className="flex gap-5">
+        <Link href="/#new-album" className="hover:underline cursor-pointer">
+          New Album
+        </Link>
+        <Link href="/#discography" className="hover:underline cursor-pointer">
+          Discography
+        </Link>
+        <Link href="/#social-media" className="hover:underline cursor-pointer">
+          Social Media
+        </Link>
       </div>
     </header>
   );
