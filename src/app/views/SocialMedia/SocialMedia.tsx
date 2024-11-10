f79625f6ae347eaa6446a04b079cd6256a61162f
josua-stng <josua.sitanggang@radyalabs.id>
2024-11-10 09:12:17 +0700

    fix: hover color for media social page

--- src/app/views/SocialMedia/SocialMedia.tsx
+++ src/app/views/SocialMedia/SocialMedia.tsx
@@ -3,22 +3,22 @@ import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa';
 import { FaSquareXTwitter } from 'react-icons/fa6';
 export default function SocialMedia() {
   return (
-    <div className="bg-gradient-to-r from-gray-400 to-zinc-200 p-10 mb-10">
+    <div className="bg-gradient-to-r from-gray-400 to-zinc-200 p-10 mb-10" id='social-media'>
       <h1 className="font-bold ml-2 text-2xl italic mb-12 text-center ">
         Follow Eminem Us Here!
       </h1>
       <div className="flex items-center justify-center text-4xl space-x-8 mb-5">
         <Link href={'https://www.facebook.com/eminem/'} target="_blank">
-          <FaFacebook className="cursor-pointer hover:text-gray-500" />
+          <FaFacebook className="cursor-pointer hover:text-blue-700" />
         </Link>
         <Link href={'https://www.instagram.com/eminem/'} target="_blank">
-          <FaInstagram className="cursor-pointer hover:text-gray-500" />
+          <FaInstagram className="cursor-pointer hover:text-red-500" />
         </Link>
         <Link href={'https://x.com/Eminem'} target="_blank">
-          <FaSquareXTwitter className="cursor-pointer hover:text-gray-500" />
+          <FaSquareXTwitter className="cursor-pointer hover:text-gray-700" />
         </Link>
         <Link href={'https://www.youtube.com/@eminem'} target="_blank">
-          <FaYoutube className="cursor-pointer hover:text-gray-500" />
+          <FaYoutube className="cursor-pointer hover:text-red-600" />
         </Link>
       </div>
     </div>
