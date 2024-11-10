9380e009d9b758f464f8d962a02eeba7cf128dcd
josua-stng <josua.sitanggang@radyalabs.id>
2024-11-10 09:16:00 +0700

    fix: remove header & footer in root page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 import Discography from './views/Discography';
-import Footer from './views/Footer';
-import HeaderAlbum from './views/HeaderAlbum';
 import LatestAlbum from './views/LatestAlbum';
 import SocialMedia from './views/SocialMedia';
 
 export default function Home() {
   return (
     <div>
-      <HeaderAlbum />
       <LatestAlbum />
       <Discography />
       <SocialMedia />
-      <Footer />
     </div>
   );
 }
